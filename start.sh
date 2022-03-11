2d6e9e2d931502f8458fc9b3d33b6fc59a1f0c3a
Navaneeth R Krishna <98873149+navaneethrkrishna007@users.noreply.github.com>
2022-03-11 15:06:15 +0530

    Bu
    Bue

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
 fi
-cd /Ajax
+cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting LuciferMoringstar RoBot...."
 python3 main.py
